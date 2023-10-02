30cf704beb0bbde93c8586a16ed905e2aeb4835e
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-02 12:50:23 +0200

    [front] add mega-menu component --template

--- frontend/src/app/home/pages/home/home.component.ts
+++ frontend/src/app/home/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MegaMenuComponent } from "../../../shared/components/mega-menu/mega-menu.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    standalone: true
+    standalone: true,
+    imports: [MegaMenuComponent]
 })
 export class HomeComponent {
 
---
+++ frontend/src/app/shared/components/mega-menu/mega-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mega-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mega-menu.component.html'
+})
+export class MegaMenuComponent {
+
+}
