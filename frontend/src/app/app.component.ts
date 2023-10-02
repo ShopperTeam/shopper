29772717f18da0f390a5325f769e4955793750e1
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-02 12:26:12 +0200

    [front] install flowbite UI library(tailwind)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
