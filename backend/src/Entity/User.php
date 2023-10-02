95c017a6e36f084f454bc3d4ea1b14c17daf178f
farnooshdev <farnooshdev@gmail.com>
2023-10-02 18:55:41 +0200

    [back] never show User's password !

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -3,38 +3,59 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use App\Repository\UserRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => ['read:User:item']]),
+        new GetCollection(normalizationContext: ['groups' => ['read:User:collection']]),
+        new Put(),
+        new Delete(),
+        new Post(),
+        new Patch(),
+    ]
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read:User:collection', 'read:User:item'])]
     private ?int $id = null;
-
+    
     #[ORM\Column(length: 180, unique: true)]
+    #[Groups(['read:User:collection', 'read:User:item'])]
     private ?string $email = null;
-
+    
     #[ORM\Column]
+    #[Groups(['read:User:collection', 'read:User:item'])]
     private array $roles = [];
-
+    
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
     private ?string $password = null;
-
+    
     #[ORM\Column]
+    #[Groups(['read:User:collection', 'read:User:item'])]
     private ?\DateTimeImmutable $createdAt = null;
-
+    
     #[ORM\Column]
+    #[Groups(['read:User:collection', 'read:User:item'])]
     private ?\DateTimeImmutable $updatedAt = null;
 
     public function __construct()
