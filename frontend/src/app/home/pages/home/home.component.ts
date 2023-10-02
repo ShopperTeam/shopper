81859eeb62594b01a4c61ed2aadf678b803be897
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-02 12:50:57 +0200

    [front] add landing component --template

--- frontend/src/app/home/pages/home/home.component.ts
+++ frontend/src/app/home/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MegaMenuComponent } from "../../../shared/components/mega-menu/mega-menu.component";
+import { LandingComponent } from "../../../shared/components/landing/landing.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: true,
-    imports: [MegaMenuComponent]
+    imports: [MegaMenuComponent, LandingComponent]
 })
 export class HomeComponent {
 
