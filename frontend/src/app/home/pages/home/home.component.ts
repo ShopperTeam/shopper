bd379890cecd3e3569405c0305866ee0fedc3bd5
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-07 18:33:26 +0200

    [front] delete unused  SCSS files

--- frontend/src/app/home/pages/home/home.component.ts
+++ frontend/src/app/home/pages/home/home.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { MegaMenuComponent } from "../../../shared/components/mega-menu/mega-menu.component";
-import { LandingComponent } from "../../../shared/components/landing/landing.component";
-import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { MegaMenuComponent } from '../../../shared/components/mega-menu/mega-menu.component';
+import { LandingComponent } from '../../../shared/components/landing/landing.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: true,
-    imports: [MegaMenuComponent, LandingComponent, FooterComponent]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [MegaMenuComponent, LandingComponent, FooterComponent],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
