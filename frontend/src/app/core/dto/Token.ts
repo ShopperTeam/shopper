565aaed64b56c190a304da3c5d9761fd72ece18e
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-30 01:29:48 +0100

    [front] add Token and UserLogin dto's

---
+++ frontend/src/app/core/dto/Token.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents token-based authentication information.
+ */
+export interface Token {
+    /**
+     * The access token for authentication and authorization.
+     */
+    token: string
+}
