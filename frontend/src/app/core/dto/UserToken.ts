ce1240d622abd52c18ba8bdfb801b40f1a7acaec
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-31 20:31:06 +0100

    [front] add UserToken dto

---
+++ frontend/src/app/core/dto/UserToken.ts
@@ -0,0 +1,29 @@
+/**
+ * Represents a User Token, typically used for JSON Web Tokens (JWTs).
+ */
+export interface UserToken {
+    /**
+     * The user's email.
+     */
+    email: string
+
+    /**
+     * The "exp" field represents the expiration date of the JWT as a number (timestamp).
+     */
+    exp: number
+
+    /**
+     * The "iat" field represents the creation date of the JWT as a number (timestamp).
+     */
+    iat: number
+
+    /**
+     * The "roles" field represents the roles associated with the user.
+     */
+    roles: string[]
+
+    /**
+     * The "ip" field represents the user's IP address.
+     */
+    ip: string
+}
