2ad43f35ded5d4f4bf96f311fc150caf474c279c
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-31 20:29:34 +0100

    [front] update login

--- frontend/src/app/auth/pages/login/login.component.ts
+++ frontend/src/app/auth/pages/login/login.component.ts
@@ -1,11 +1,49 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { RouterLink } from '@angular/router'
+import { UserLogin } from '@core/dto/UserLogin'
+import { AuthService } from './../../services/auth.service'
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink, ReactiveFormsModule],
     templateUrl: './login.component.html',
     styles: [],
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+            '',
+            [
+                Validators.required,
+                // Validators.minLength(8),
+                // Validators.maxLength(40),
+                // CustomValidators.password(),
+            ],
+        ],
+    })
+
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService
+    ) {}
+
+    onSubmit() {
+        if (!this.loginForm.valid) return
+        const userLogin: UserLogin = this.loginForm.value as UserLogin
+        this.authService.login(userLogin).subscribe({
+            next: response => {
+                if (!response) {
+                    throw new Error('Vous êtes déjà connecté.')
+                }
+                console.log('Bienvenue.')
+            },
+            error: error => {
+                console.error(JSON.stringify(error))
+            },
+        })
+    }
+}
