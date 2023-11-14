45bae5bad8febab3c96804ae2e172e7b3b9fd7ce
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-14 16:46:47 +0100

    [front] rename theme-switcher to theme-widget + update

--- frontend/src/app/shared/components/mega-menu/nav-widgets/nav-widgets.component.ts
+++ frontend/src/app/shared/components/mega-menu/nav-widgets/nav-widgets.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
-import { ThemeSwitcherComponent } from '../theme-switcher/theme-switcher.component'
 import { CartWidgetComponent } from './cart-widget/cart-widget.component'
 import { FavoriteWidgetComponent } from './favorite-widget/favorite-widget.component'
+import { ThemeWidgetComponent } from './theme-widget/theme-widget.component'
 import { NavUserComponent } from './user-widget/nav-user-widget.component'
 
 @Component({
@@ -12,7 +12,7 @@ import { NavUserComponent } from './user-widget/nav-user-widget.component'
         CommonModule,
         CartWidgetComponent,
         FavoriteWidgetComponent,
-        ThemeSwitcherComponent,
+        ThemeWidgetComponent,
         NavUserComponent,
     ],
     templateUrl: './nav-widgets.component.html',
